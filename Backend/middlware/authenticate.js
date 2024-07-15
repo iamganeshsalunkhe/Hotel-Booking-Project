63df96047f671df8bfc76526d2ba48b9f278f2a0
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-07-15 23:30:25 +0530

    Add comments.

--- Backend/middlware/authenticate.js
+++ Backend/middlware/authenticate.js
@@ -2,22 +2,32 @@ const jwt = require("jsonwebtoken");
 const { Users } = require("../models"); 
 
 module.exports = async (req, res, next) => {
+    // getting token
     const token = req.header("Authorization");
+    
+    // if token not provided
     if (!token) {
     return res.status(401).json({ message: "Access denied. No token provided." });
     };
 
     try {
-        const getUserId = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await Users.findByPk(getUserId.id); 
+        // check user is valid using jwt
+        const getUser = jwt.verify(token, process.env.JWT_SECRET);
+
+        // extract user using Primary key 
+        const user = await Users.findByPk(getUser.id); 
     
+        // if user not found
         if (!user) {
         return res.status(404).json({ message: "User not found" });
     };
-
+        // attach user object to request for further routes
         req.user = user; 
+
+        //call the next function
         next();
     } catch (error) {
+        // if invalid token provided
     res.status(400).json({ message: "Invalid token." });
     }
 };
