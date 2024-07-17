84b750d8efa792399297544f079014d821bf0c30
Ganesh  Salunkhe <ganeshsalunkhe1998@gmail.com>
2024-07-17 16:40:19 +0530

    update and delete an amenity routes add

--- Backend/controller/amenitiesController.js
+++ Backend/controller/amenitiesController.js
@@ -34,4 +34,60 @@ exports.createAmenity = async (req,res)=>{
         // if any error occurs
         res.status(500).json({message:"Internal server error"});
     }
-}
+};
+
+// update an existing amenity
+
+exports.updateAmenity = async(req,res) =>{
+    try {
+        // get id 
+        const {amenityId} = req.params;
+        // get input from user
+        const {name,description, price} = req.body;
+
+        //serach for amenity using amenityId
+        const existingamenity = await amenities.findByPk(amenityId);
+        
+        // update the amenity
+        // if name not provided
+        existingamenity.name = name || existingamenity.name;
+        // if description not provided
+        existingamenity.description = description || existingamenity.description;
+
+        // if price not provided
+        existingamenity.price = price || existingamenity.price;
+
+        await existingamenity.save();
+
+        // response on success
+        res.status(200).json(existingamenity);
+
+    } catch (error) {
+        // if any error occurs
+        res.status(500).json({message:"Internal error"});
+    }
+};
+
+// delete an existing amenity
+exports.deleteAmenity = async (req,res)=>{
+    try {
+        // get amenityId from params
+        const {amenityId} = req.params;
+        
+        // get amenity by Id
+        const todeleteamenity = await amenities.findByPk(amenityId);
+
+        // if amenity not found
+        if (!todeleteamenity) return res.status(404).json({message:'Amenity not found'})
+        
+        // delete the amenity
+        await todeleteamenity.destroy();
+  
+        // response on success
+        res.status(200).json({message:"Amenity deleted successfully"});
+
+    } catch (error) {
+        // if any error occurs
+        res.status(500).json({message:"Internal server error"});
+    }
+};
--- Backend/routes/amenities.js
+++ Backend/routes/amenities.js
@@ -12,6 +12,8 @@ router.get('/amenities',authenticate,amenityController.getAllamenities);
 // to add a new amenity
 router.post('/amenities',authenticate,amenityController.createAmenity);
 
+router.put('/amenities/:amenityId',authenticate,amenityController.updateAmenity);
 
+router.delete('/amenities/:amenityId',authenticate,amenityController.deleteAmenity);
 
 module.exports = router;
